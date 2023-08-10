8dabd1cc6ce67acd365cfcee9c324bfcf6489eb0
hoangbao0201 <hoangbao020103@gmail.com>
2023-08-10 23:15:27 +0700

    Fix Deploy

--- src/pages/bai-viet/[slug].tsx
+++ src/pages/bai-viet/[slug].tsx
@@ -1,13 +1,14 @@
-import { Fragment, ReactNode } from "react";
+import { ReactNode } from "react";
 import { GetStaticPaths, GetStaticProps } from "next";
 
+import { marked } from "marked";
 import { Params } from "@/config";
 import { BlogsTypes } from "@/types";
 
-import MainLayout from "@/components/layouts/MainLayout";
-import blogService from "@/services/blog.services";
+import formatDate from "@/utils/formatDate";
 import { NextPageWithLayout } from "../_app";
-import { marked } from "marked";
+import blogService from "@/services/blog.services";
+import MainLayout from "@/components/layouts/MainLayout";
 
 interface BlogDetailPageProps {
     blog: BlogsTypes | null;
@@ -23,6 +24,16 @@ const BlogDetailPage: NextPageWithLayout<BlogDetailPageProps> = ({ blog }) => {
                 {
                     blog && (
                         <>
+                            <div>
+                                <dl className="mb-1">
+                                    <dt></dt>
+                                    <dd>
+                                        <time dateTime={`${blog.createdAt}`}>{formatDate(String(blog.createdAt))}</time>
+                                    </dd>
+                                    <h1 className="text-4xl">{blog.title}</h1>
+                                </dl>
+                                <div></div>
+                            </div>
                             <div className="prose" dangerouslySetInnerHTML={{ __html: marked(blog.content) }} />
                         </>
                     )
