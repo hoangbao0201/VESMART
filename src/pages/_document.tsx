87b190873a0c21d8f96babd54bb40399cf44e959
hoangbao0201 <hoangbao020103@gmail.com>
2023-08-11 16:14:45 +0700

    Add meta google search

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="9u_83za3WMns-7aZvdsjOW6Rm_lUD2VIiAEaKbzfdGw" />
+      </Head>
       <body>
         <Main />
         <NextScript />
